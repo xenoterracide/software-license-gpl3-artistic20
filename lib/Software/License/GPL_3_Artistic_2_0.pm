941befe668b1cfded1a57e70ae74377cf81846fb
Caleb Cushing <xenoterracide@gmail.com>
2010-05-05 13:43:41 -0400

    attempt a GPL3 Artistic 2.0 license

--- lib/Software/License/GPL_3_Artistic_2_0.pm
+++ lib/Software/License/GPL_3_Artistic_2_0.pm
@@ -5,20 +5,20 @@ use base 'Software::License';
 
 =head1 NAME
 
-Software::License::Perl_5 - The Perl 5 License (Artistic 1 & GPL 1)
+Software::License::GPL_3_Artistic_2_0 - GPL 3 and Artistic 2.0
 
 =cut
 
-require Software::License::GPL_1;
-require Software::License::Artistic_1_0;
+require Software::License::GPL_3;
+require Software::License::Artistic_2_0;
 
-sub name { 'the same terms as the perl 5 programming language system itself' }
-sub url  { 'http://dev.perl.org/licenses/' }
+sub name { 'GNU GPLv3 and Artistic 2.0' }
+sub url  { 'http://www.gnu.org/licenses/gpl-3.0.txt http://www.perlfoundation.org/artistic_license_2_0' }
 sub meta_name { 'perl' }
 
 sub _gpl {
   my ($self) = @_;
-  return $self->{_gpl} ||= Software::License::GPL_1->new({
+  return $self->{_gpl} ||= Software::License::GPL_3->new({
     year   => $self->year,
     holder => $self->holder,
   });
@@ -26,7 +26,7 @@ sub _gpl {
 
 sub _tal {
   my ($self) = @_;
-  return $self->{_tal} ||= Software::License::Artistic_1_0->new({
+  return $self->{_tal} ||= Software::License::Artistic_2_0->new({
     year   => $self->year,
     holder => $self->holder,
   });
@@ -38,14 +38,13 @@ __NOTICE__
 This software is copyright (c) {{$self->year}} by {{$self->holder}}.
 
 This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-__LICENSE__
-Terms of the Perl programming language system itself
+one of the following licenses
 
 a) the GNU General Public License as published by the Free
-   Software Foundation; either version 1, or (at your option) any
+   Software Foundation; either version 3, or (at your option) any
    later version, or
-b) the "Artistic License"
+b) the "Artistic License 2.0"
+__LICENSE__
 
 --- {{ $self->_gpl->name }} ---
 
